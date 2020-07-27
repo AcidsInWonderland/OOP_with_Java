ec8535e903d73a4a6407c02f4b88e44244d76864
Robyn Lesch <67701622+AcidsInWonderland@users.noreply.github.com>
2020-07-27 02:32:48 +0200

    Create Car.java

---
+++ Polymorphism and Inheritance/Vehicle.java/Car.java
@@ -0,0 +1,15 @@
+public class Car extends Vehicle
+{
+    int doors;
+    // System.out.println (new Car ("Black", 4, 4));
+    public Car(String color,int passenger,int doors)
+    {
+        super(passenger,color);
+        this.doors=doors;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+" "+doors+" doors";
+    }
+}
