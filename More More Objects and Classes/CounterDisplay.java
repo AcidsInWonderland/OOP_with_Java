481a2b971b017b484fffad52552c1ad3642458bd
Robyn Lesch <67701622+AcidsInWonderland@users.noreply.github.com>
2020-07-27 02:56:35 +0200

    Create CounterDisplay.java

---
+++ More More Objects and Classes/CounterDisplay.java
@@ -0,0 +1,30 @@
+// class counter display
+public class CounterDisplay{ 
+	private int value;
+	private int limit; 
+	public CounterDisplay(int pLimit)
+	{
+		value=0;
+		limit = pLimit; 		
+	}
+	public void setValue(int pValue)
+	{
+      value = pValue;
+	}
+	public int getValue()
+	{
+		return value;
+	}
+	public void increment() 
+	{
+	value = value+1; 
+	if (value>=limit)
+		value = 0; 
+	}
+	public String getDisplayValue()
+	{
+		String counterStr;
+		counterStr = "0" + getValue(); 
+		return counterStr; 
+	}
+}
